cb77a15d93115b3123fabf2d69ac3602e5137249
Juan Arias <juan.arias.freire@bbva.com>
2021-09-12 22:13:00 +0200

    Rename honours extension

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,6 +16,9 @@ def test_rename_list(tmpdir):
     with pytest.raises(InvalidEpisodeCountError):
         rename_list(["a", "b"], [("S1", 3)])
 
+    with pytest.raises(InvalidEpisodeCountError):
+        rename_list(["a", "b", "c", "d"], [("S1", 1), ("S2", 3), ("S3", 1)])
+
     assert rename_list(["a", "b", "c"], [("S01", 3)]) == [
         "S01E01",
         "S01E02",
@@ -34,4 +37,18 @@ def test_rename_list(tmpdir):
         "S01E01.mkv",
         "S01E02.mkv",
         "S01E03.mkv",
+    ]
+
+    assert rename_list([
+        "Dragon_Ball_1024x768_1.mkv",
+        "Dragon_Ball_1024x768_2.mkv",
+        "Dragon_Ball_1024x768_3.mkv",
+        "Dragon_Ball_1024x768_4.mkv",
+        "Dragon_Ball_1024x768_5.mkv",
+    ], [("S1", 3), ("S2", 2)]) == [
+        "S01E01.mkv",
+        "S01E02.mkv",
+        "S01E03.mkv",
+        "S02E01.mkv",
+        "S02E02.mkv",
     ]
