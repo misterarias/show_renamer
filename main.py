113f45d3f72949875997ca46b89c3dbd5aacad06
Juan Arias <juan.arias.freire@bbva.com>
2021-09-12 22:03:18 +0200

    Rename based on sequence

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from typing import List, Tuple
 
 
@@ -5,8 +6,21 @@ class InvalidEpisodeCountError(Exception):
     pass
 
 
+class InvalidSeasonFormatError(Exception):
+    pass
+
+
 def main():
     pass
+
+
+def extract_season_number(season: str) -> int:
+    number = re.search(r'.*(\d+).*', season)
+    print(number)
+    if not number:
+        raise InvalidSeasonFormatError()
+
+    return int(number.group(1))
 
 
 def rename_list(file_list: list, season_definition: List[Tuple[str, int]]) -> list:
@@ -20,7 +34,8 @@ def rename_list(file_list: list, season_definition: List[Tuple[str, int]]) -> li
 
     sequential_definitions = []
     for (season, episode_count) in season_definition:
-        sequential_definitions.extend([f"{season}E{i:02}" for i in range(1, episode_count + 1)])
+        season_number = extract_season_number(season)
+        sequential_definitions.extend([f"S{season_number:02}E{i:02}" for i in range(1, episode_count + 1)])
 
     return sequential_definitions
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
 import pytest
 
-from main import rename_list, InvalidEpisodeCountError
+from main import rename_list, InvalidEpisodeCountError, extract_season_number
+
+
+def test_extract_season_number():
+    assert extract_season_number("S1") == 1
+    assert extract_season_number("S01") == 1
+    assert extract_season_number("Season 1") == 1
 
 
 def test_rename_list(tmpdir):
@@ -11,6 +17,11 @@ def test_rename_list(tmpdir):
         rename_list(["a", "b"], [("S1", 3)])
 
     assert rename_list(["a", "b", "c"], [("S01", 3)]) == [
+        "S01E01",
+        "S01E02",
+        "S01E03",
+    ]
+    assert rename_list(["a", "b", "c"], [("S1", 3)]) == [
         "S01E01",
         "S01E02",
         "S01E03",
