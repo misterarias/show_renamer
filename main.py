8a65ab58e29a98bf8a423d45cb0a4376205bc64b
Juan Arias <juan.arias.freire@bbva.com>
2021-09-12 13:07:08 +0200

    Rename based on season to episode count

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 class InvalidEpisodeCountError(Exception):
     pass
 
@@ -6,14 +9,20 @@ def main():
     pass
 
 
-def rename_list(file_list: list, season_definition: dict) -> list:
+def rename_list(file_list: list, season_definition: List[Tuple[str, int]]) -> list:
     if season_definition is None:
         return file_list
 
-    total_definitions = sum(len(episodes) for _, episodes in season_definition.items())
+    total_definitions = sum(episode_count for (_, episode_count) in season_definition)
     total_files = len(file_list)
     if total_definitions != total_files:
         raise InvalidEpisodeCountError()
+
+    sequential_definitions = []
+    for (season, episode_count) in season_definition:
+        sequential_definitions.extend([f"{season}E{i:02}" for i in range(1, episode_count + 1)])
+
+    return sequential_definitions
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,5 +8,10 @@ def test_rename_list(tmpdir):
     assert rename_list(["a", "b", "c"], None) == ["a", "b", "c"]
 
     with pytest.raises(InvalidEpisodeCountError):
-        rename_list(["a", "b"], {"S01": ["1", "2", "3"]})
+        rename_list(["a", "b"], [("S1", 3)])
 
+    assert rename_list(["a", "b", "c"], [("S01", 3)]) == [
+        "S01E01",
+        "S01E02",
+        "S01E03",
+    ]
