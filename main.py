c8973be7c6e6f2926bf83e8d0575961b51e6b4f1
Juan Arias <juan.arias.freire@bbva.com>
2021-09-12 12:58:31 +0200

    Fail if count does not match

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+class InvalidEpisodeCountError(Exception):
+    pass
+
+
 def main():
     pass
 
@@ -6,6 +10,11 @@ def rename_list(file_list: list, season_definition: dict) -> list:
     if season_definition is None:
         return file_list
 
+    total_definitions = sum(len(episodes) for _, episodes in season_definition.items())
+    total_files = len(file_list)
+    if total_definitions != total_files:
+        raise InvalidEpisodeCountError()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
-from main import rename_list
+import pytest
+
+from main import rename_list, InvalidEpisodeCountError
 
 
 def test_rename_list(tmpdir):
     assert rename_list([], None) == []
-    assert rename_list(['a', 'b', 'c'], None) == ['a', 'b', 'c']
+    assert rename_list(["a", "b", "c"], None) == ["a", "b", "c"]
+
+    with pytest.raises(InvalidEpisodeCountError):
+        rename_list(["a", "b"], {"S01": ["1", "2", "3"]})
+
